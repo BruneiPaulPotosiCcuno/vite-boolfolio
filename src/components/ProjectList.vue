df563044480dc54957fbe20bdd29aaeeca23437f
BruneiPaulPotosiCcuno <neoxtrotoka@gmail.com>
2024-06-16 20:26:05 +0200

    add-scaffolding-and-boostrap

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: 'ProjectList'
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1>Questi sono tutti i post:</h1>
+        <div class="row row-cols-3">
+            <div class="card">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+
+</style>
